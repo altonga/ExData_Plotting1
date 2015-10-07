dd1f8c8d2fc916078c01d1a0280c1bc4fc16d93a
altonga <altonga@gmail.com>
2015-10-07 15:44:33 -0700

    Cleaned up Plot 2

--- plot2.R
+++ plot2.R
@@ -10,9 +10,7 @@ data <- read.csv("household_power_consumption.txt", sep = ";", header = TRUE,
                  colClasses = c("character", "character", rep("numeric", 7)), 
                  na.strings=c("?"))
 data[,1] <- as.Date(data[,1], "%d/%m/%Y")
-#data[,2] <- strptime(data[,2], "%H:%M:%S")
 data$datetime <-paste(data$Date, data$Time) #, "%Y-%m-%d %H:%M:%S")
-#data[, 1] <- as.Date(data[,1]) 
 plotdata <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02")
 png("plot2.png", width = 480, height = 480)
 plot(strptime(plotdata$datetime, "%Y-%m-%d %H:%M:%S"), plotdata$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
